c9e0444f3a73ca9297606a4e8ec46234cc5f7ab9
wojciechprazuch <wojciechprazuch3@gmail.com>
2019-06-21 23:20:46 +0200

    FindDuplicates Code Wars kata

---
+++ FindDuplicates.py
@@ -0,0 +1,17 @@
+## https://www.codewars.com/kata/5558cc216a7a231ac9000022
+
+import unittest
+
+def duplicates(array):
+    duplicates = []
+    for i in range(len(array)):
+        if array[i] in array[:i] and array[i] not in duplicates:
+            duplicates.append(array[i])
+    return duplicates
+
+
+
+print(duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3, '5']))
+
+
+
