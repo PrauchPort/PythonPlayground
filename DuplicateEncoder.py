b475fa55a5f1d82256711f21f3362d4aaff81e80
wojciechprazuch <wojciechprazuch3@gmail.com>
2019-06-22 11:51:16 +0200

    Updated minimized implementation

--- DuplicateEncoder.py
+++ DuplicateEncoder.py
@@ -13,6 +13,10 @@ def duplicate_encode(word):
     return output
 
 
+def duplicate_encode2(word):
+    return ''.join(['(' if word.lower().count(c) == 1 else ')' for c in word.lower()])
+
+
 test = unittest.TestCase()
 
 test.assertEquals(duplicate_encode("din"), "(((")
@@ -21,5 +25,11 @@ test.assertEquals(duplicate_encode("Success"), ")())())", "should ignore case")
 test.assertEquals(duplicate_encode("(( @"), "))((")
 test.assertEquals(duplicate_encode("nz Ik@Qk!xQRRb(GQIR"), "((())())(()))((()))")
 
+
+test.assertEquals(duplicate_encode2("din"), "(((")
+test.assertEquals(duplicate_encode2("recede"), "()()()")
+test.assertEquals(duplicate_encode2("Success"), ")())())", "should ignore case")
+test.assertEquals(duplicate_encode2("(( @"), "))((")
+test.assertEquals(duplicate_encode2("nz Ik@Qk!xQRRb(GQIR"), "((())())(()))((()))")
 
 
